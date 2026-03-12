9f137cf559274c442809a894fd039a62054ba87b
Irene <iandpin0711@g.educaand.es>
2026-03-12 12:27:10 +0100

    Add volume control slider in main menu
    - Added HSlider to adjust global volume
    - Slider updates music volume in real time

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,7 +1,17 @@
 extends Control
 
 @onready var music = $MenuMusic
+@onready var h_slider = $HSlider
+
+func _ready():
+	var linear_vol = db_to_linear(AudioServer.get_bus_volume_db(0))
+	h_slider.value = linear_vol
 
 func _on_texture_button_pressed():
 	music.stop()
 	get_tree().change_scene_to_file("res://Scene/MainScene.tscn")
+
+func _on_h_slider_value_changed(value: float) -> void:
+	var adjusted_value = clamp(value, 0.0001, 1.0)
+	music.volume_db = linear_to_db(adjusted_value)
+	AudioServer.set_bus_volume_db(0, linear_to_db(adjusted_value))
